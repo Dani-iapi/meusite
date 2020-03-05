1e720644aeaa5285888f2a3ad1117b67c4332533
Daniela Rodrigues de Araujo <1901571@SALAS.AULAS>
2020-03-04 21:42:32 -0300

    a

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello from Dani com sono!</h1>'
+    return 'Hello da Dani!'
+
+    @app.route('/')
+def inicio():
+    return render_template('index.html')
+
 
